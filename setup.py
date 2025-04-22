562e2d53547e9707b431a4a7029e4f02c30e4f64
AYUSH AGARWAL <aa3022@srmist.edu.in>
2025-04-22 17:15:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ayush Agarwal',
+    author_email= 'firstgeek31@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
